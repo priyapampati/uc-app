347de146d709d27902942010f71000a926151fc3
Lakshmi Priya Pampati <45875844+priyapampati@users.noreply.github.com>
2020-06-28 09:36:40 +0530

    Add files via upload

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,23 @@
+import React,{ createContext , useState} from 'react'
+
+export const BookContext = createContext();
+
+
+const BookContextProvider = (props) => {
+  const [ books , setBooks] = useState([
+        {title : 'Think and Grow Rich',id  : 1},
+        {title : 'The Great Gatsby', id: 2},
+        {title : ' Rich Dad Poor Dad',id : 3},
+        {title: ' How to win friends and influence people',id : 4},
+    ])
+
+
+    return (<div>
+        <BookContext.Provider value={ {books} }>
+        {props.children}
+        </BookContext.Provider>
+        </div>
+    );
+}
+ 
+export default BookContextProvider;
